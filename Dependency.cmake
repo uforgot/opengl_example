a8c12f989d2dcfe1e91f3aad551b37202d54564d
uforgot <uforgot@me.com>
2024-01-02 12:55:36 +0900

    [add] default init

---
+++ Dependency.cmake
@@ -0,0 +1,38 @@
+# ExternalProject 관련 명령어 셋추가
+include(ExternalProject)
+
+#Dependency 관련 변수 설정
+set(DEP_INSTALL_DIR ${PROJECT_BINARY_DIR}/install)
+set(DEP_INCLUDE_DIR ${DEP_INSTALL_DIR}/include)
+set(DEP_LIB_DIR ${DEP_INSTALL_DIR}/lib)
+
+#spdlog: fast logger library
+ExternalProject_Add(
+    dep_spdlog
+    GIT_REPOSITORY "https://github.com/gabime/spdlog.git"
+    GIT_TAG "v1.x"
+    GIT_SHALLOW 1
+    UPDATE_COMMAND "" PATCH_COMMAND "" TEST_COMMAND ""
+    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${DEP_INSTALL_DIR}
+)
+
+set(DEP_LIST ${DEP_LIST} dep_spdlog)
+# 왜인지 모르겠으나 맥에서는 안됨
+# set(DEP_LIBS ${DEP_LIBS} spdlog$<$<CONFIG:Debug>:d>)
+set(DEP_LIBS ${DEP_LIBS} spdlog)
+
+ExternalProject_Add(
+    dep_glfw
+    GIT_REPOSITORY "https://github.com/glfw/glfw.git"
+    GIT_TAG "3.3.9"
+    GIT_SHALLOW 1
+    UPDATE_COMMAND "" PATCH_COMMAND "" TEST_COMMAND ""
+    CMAKE_ARGS 
+        -DCMAKE_INSTALL_PREFIX=${DEP_INSTALL_DIR}
+        -DGLFW_BUILD_EXAMPLES=OFF
+        -DGLFW_BUILD_TESTS=OFF
+        -DGLFW_BUILD_DOCS=OFF
+)
+
+set(DEP_LIST ${DEP_LIST} dep_glfw)
+set(DEP_LIBS ${DEP_LIBS} glfw3)
---
+++ src/main.cpp
@@ -0,0 +1,41 @@
+/* Ask for an OpenGL Core Context */
+#define GLFW_INCLUDE_GLCOREARB
+
+#include <iostream>
+#include <spdlog/spdlog.h>
+#include <GLFW/glfw3.h>
+
+int main(int argc, const char** argv) {
+    SPDLOG_INFO("Start program");
+    
+    // #ifdef __APPLE__
+    // /* We need to explicitly ask for a 3.2 context on OS X */
+    // glfwWindowHint (GLFW_CONTEXT_VERSION_MAJOR, 3);
+    // glfwWindowHint (GLFW_CONTEXT_VERSION_MINOR, 2);
+    // glfwWindowHint (GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    // glfwWindowHint (GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // #endif 
+
+    if (!glfwInit()) {
+        const char* description = nullptr;
+        glfwGetError(&description);
+        SPDLOG_ERROR("failed to initialize glfw: {}", description);
+        return -1;
+    }
+
+    SPDLOG_INFO("Create glfw window");
+    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME, nullptr, nullptr);
+    if (!window) {
+        SPDLOG_ERROR("failed to create glfw window");
+        glfwTerminate();
+        return -1;
+    }
+
+    SPDLOG_INFO("Start main loop");
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
