ed67d3a17637e7da06a75740cad4d8abd4921abc
uforgot <uforgot@me.com>
2024-01-02 16:21:43 +0900

    [add] gl interface callback

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,33 @@
 #include <GLFW/glfw3.h>
 
 void OnFramebufferSizeChanged(GLFWwindow* window, int width, int height) {
-    SPDLOG_INFO("Framebuffer size changed: {}x{}", width, height);
+    SPDLOG_INFO("Framebuffer size changed: {} x {}", width, height);
     glViewport(0, 0, width, height);
+}
+
+void OnKeyEvent(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    SPDLOG_INFO("Key: {}, scancode: {}, action: {} mods: {}{}{}", 
+        key, scancode, 
+        action == GLFW_PRESS ? "Pressed" : 
+        action == GLFW_RELEASE ? "Released" : 
+        action == GLFW_REPEAT ? "Repeat" : "Unknown", 
+        mods & GLFW_MOD_CONTROL ? "C" : "-",
+        mods & GLFW_MOD_SHIFT ? "S" : "-",
+        mods & GLFW_MOD_ALT ? "A" : "-"
+        );
+
+    if (key == GLFW_KEY_ESCAPE) {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+}
+
+void Render() {
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear (GL_COLOR_BUFFER_BIT);
 }
 
 int main(int argc, const char** argv) {
     SPDLOG_INFO("Start program");
-    
-    // #ifdef __APPLE__
-    // /* We need to explicitly ask for a 3.2 context on OS X */
-    // glfwWindowHint (GLFW_CONTEXT_VERSION_MINOR, 2);
-    // glfwWindowHint (GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    // glfwWindowHint (GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    // #endif 
 
     if (!glfwInit()) {
         const char* description = nullptr;
@@ -50,14 +64,18 @@ int main(int argc, const char** argv) {
 
     auto glVersion = glGetString(GL_VERSION);
     //SPDLOG_INFO("GL Version: {}", glVersion); < 이상하게 fmt error가 난다.
-    std::cout << "GL Version: " << glVersion << std::endl;        
+    std::cout << "--> GL Version: " << glVersion << std::endl;        
 
+    OnFramebufferSizeChanged(window, WINDOW_WIDTH, WINDOW_HEIGHT);
     glfwSetFramebufferSizeCallback(window, OnFramebufferSizeChanged);
-    
+    glfwSetKeyCallback(window, OnKeyEvent);
 
     // SPDLOG_INFO("Start main loop");
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
+        glClearColor(0.0f, 0.1f, 0.2f, 0.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
     }
 
     glfwTerminate();
