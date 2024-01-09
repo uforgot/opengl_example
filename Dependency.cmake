224cfa3b0ed10fcabcd24414c08940798f4721b8
uforgot <uforgot@me.com>
2024-01-09 15:00:37 +0900

    [add] glm

--- Dependency.cmake
+++ Dependency.cmake
@@ -63,3 +63,14 @@ ExternalProject_Add(
 )
 
 set(DEP_LIST ${DEP_LIST} dep_stb)
+
+ExternalProject_Add(
+    dep_glm
+    GIT_REPOSITORY "https://github.com/g-truc/glm.git"
+    GIT_TAG "0.9.9.8"
+    GIT_SHALLOW 1
+    UPDATE_COMMAND "" PATCH_COMMAND "" CONFIGURE_COMMAND "" BUILD_COMMAND "" TEST_COMMAND ""
+    INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_directory ${PROJECT_BINARY_DIR}/dep_glm-prefix/src/dep_glm/glm ${DEP_INSTALL_DIR}/include/glm
+)
+
+set(DEP_LIST ${DEP_LIST} dep_glm)
