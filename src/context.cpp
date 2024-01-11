2a2adc1b32172c5598c7719eb7abd7e30a90483c
uforgot <uforgot@me.com>
2024-01-11 11:38:48 +0900

    [add] camera transform 추가

--- src/context.cpp
+++ src/context.cpp
@@ -132,7 +132,25 @@ void Context::Render() {
     glEnable(GL_DEPTH_TEST);
 
     auto projection = glm::perspective(glm::radians(45.0f), (float)WINDOW_WIDTH / (float)WINDOW_HEIGHT, 0.1f, 100.0f);
-    auto view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f,0.0f,-3.0f));
+    float x = sinf((float)glfwGetTime() * glm::pi<float>() * 2.0f) * 3.0f;
+    // auto view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f,0.0f,-3.0f));
+
+    auto cameraPos = glm::vec3(x, 0.0f, 3.0f);
+    auto cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+    auto cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+    auto cameraZ = glm::normalize(cameraPos - cameraTarget);
+    auto cameraX = glm::normalize(glm::cross(cameraUp, cameraZ));
+    auto cameraY = glm::cross(cameraZ, cameraX);
+
+    auto cameraMat = glm::mat4(
+        glm::vec4(cameraX, 0.0f),
+        glm::vec4(cameraY, 0.0f),
+        glm::vec4(cameraZ, 0.0f),
+        glm::vec4(cameraPos, 1.0f)
+    );
+
+    auto view = glm::inverse(cameraMat);
 
     for (size_t i=0;i<cubePositions.size();i++) {
         auto& pos = cubePositions[i];
@@ -143,6 +161,7 @@ void Context::Render() {
             glm::vec3(1.0f, 0.5f, 0.0f)
         );
         auto transform = projection * view * model;
+
         m_program->SetUniform("transform", transform);
         glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
     }
